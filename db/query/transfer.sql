c652f80dcce8e874395224f5ee6401a7667fd5a4
Alikhan <dosimovic04@gmail.com>
2024-06-29 11:24:12 +0500

    transfer table crud

--- db/query/transfer.sql
+++ db/query/transfer.sql
@@ -10,3 +10,21 @@ INSERT INTO transfers (
 -- name: GetTransfer :one
 SELECT * FROM transfers
 WHERE id = $1 LIMIT 1;
+
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+ORDER BY id
+OFFSET $2
+LIMIT $1;
+
+-- name: UpdateTransfer :one
+UPDATE transfers
+set amount = $2
+WHERE id = $1
+RETURNING *;
+
+
+-- name: DeleteTransfer :exec
+DELETE FROM transfers
+WHERE id = $1;
