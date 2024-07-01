87f9a5668d7edce1a08b87d8b924854c0ada05f1
Alikhan <dosimovic04@gmail.com>
2024-07-01 12:38:20 +0500

    transaction test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -14,16 +14,19 @@ const (
 	dataSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
+var testDB *sql.DB
+
 var testqueries *Queries
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(drivername, dataSource)
+	var err error
+	testDB, err = sql.Open(drivername, dataSource)
 
 	if err != nil {
 		log.Fatal("err to connect database", err)
 	}
 
-	testqueries = New(conn)
+	testqueries = New(testDB)
 
 	os.Exit(m.Run())
 }
